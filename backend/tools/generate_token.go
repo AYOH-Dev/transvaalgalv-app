b9e7f5de84c91e01ea225efcd2ebcbc7cc0c9d2c
dane-ayoh <dane@ayoh.group>
2026-05-06 16:04:35 +0000

    fix: remove duplicate package declaration in generate_token.go

--- backend/tools/generate_token.go
+++ backend/tools/generate_token.go
@@ -1,31 +1,25 @@
-package tools
 package main
 
 import (
-    "fmt"
-    "os"
-    "time"
+	"fmt"
+	"os"
+	"time"
 
-    "github.com/AYOH-Dev/transvaalgalv-app/backend/internal/auth"
+	"github.com/AYOH-Dev/transvaalgalv-app/backend/internal/auth"
 )
 
 func main() {
-    if len(os.Args) < 5 {
-        fmt.Fprintf(os.Stderr, "usage: generate_token <secret> <user_id> <email> <role>\n")
-        os.Exit(2)
-    }
+	if len(os.Args) < 5 {
+		fmt.Fprintf(os.Stderr, "usage: generate_token <secret> <user_id> <email> <role>\n")
+		os.Exit(2)
+	}
 
-    secret := os.Args[1]
-    userID := os.Args[2]
-    email := os.Args[3]
-    role := os.Args[4]
+	secret := os.Args[1]
+	userID := os.Args[2]
+	email := os.Args[3]
+	role := os.Args[4]
 
-    tm := auth.NewTokenManager(secret, 24*time.Hour)
-    token, exp, err := tm.Generate(auth.Subject{UserID: userID, Email: email, Role: role})
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "error: %v\n", err)
-        os.Exit(1)
-    }
-
-    fmt.Printf("token=%s\nexpires_at=%s\n", token, exp.UTC().Format(time.RFC3339))
-}
+	tm := auth.NewTokenManager(secret, 24*time.Hour)
+	token, exp, err := tm.Generate(auth.Subject{UserID: userID, Email: email, Role: role})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: 
